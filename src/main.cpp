c35c637496b0f124242992a33a446a20f4dbe12a
Nick Dumas <nick.dumas@airo.net.au>
2026-06-18 21:41:24 +0800

    Fix movement
    Still needs better caster wheel or omniwheel to work

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,18 @@
 #include <Wire.h>
 #include <Adafruit_TCS34725.h>
 
-// -------------------- Pin definitions --------------------
+// Types
 
+struct MotorPins {
+  uint8_t fwd;
+  uint8_t bwd;
+  uint8_t pwm;
+};
 
-#define MTR_R_PWM 32
-#define MTR_R_FWD 33
-#define MTR_R_BWD 25
-#define MTR_L_FWD 26
-#define MTR_L_BWD 27
-#define MTR_L_PWM 14
+// -------------------- Pins --------------------
+
+MotorPins MOTOR_R = {25, 33, 32};
+MotorPins MOTOR_L = {26, 27, 14};
 
 #define CLR_L_SDA 18
 #define CLR_L_SCL 5
@@ -20,47 +23,36 @@
 #define CLR_R_SDA 17
 #define CLR_R_SCL 16
 
+TwoWire I2C_L(0);
+TwoWire I2C_R(1);
+  
+Adafruit_TCS34725 TCS_L(TCS34725_INTEGRATIONTIME_24MS, TCS34725_GAIN_4X);
+Adafruit_TCS34725 TCS_R(TCS34725_INTEGRATIONTIME_24MS, TCS34725_GAIN_4X);
+
 // -------------------- Tuning --------------------
 
 const uint16_t LINE_THRESHOLD = 1000;
 const int BASE = 150;
 const int TURN = 200;
-const int loopDelayMs = 20;
 
-// -------------------- Motors --------------------
+// -------------------- Functions --------------------
 
 // Left motor is physically inverted, so speed is negated before driving.
-void driveLeft(int speed) {
+void driveMotor(MotorPins pins, int speed) {
   int s = constrain(-speed, -255, 255);
-  digitalWrite(MTR_L_FWD, s < 0);
-  digitalWrite(MTR_L_BWD, s >= 0);
-  analogWrite(MTR_L_PWM, abs(s));
-}
-
-void driveRight(int speed) {
-  int s = constrain(speed, -255, 255);
-  digitalWrite(MTR_R_FWD, s < 0);
-  digitalWrite(MTR_R_BWD, s >= 0);
-  analogWrite(MTR_R_PWM, abs(s));
+  digitalWrite(pins.fwd, s < 0);
+  digitalWrite(pins.bwd, s >= 0);
+  analogWrite(pins.pwm, abs(s));
 }
 
 void driveMotors(int leftSpeed, int rightSpeed) {
-  driveLeft(leftSpeed);
-  driveRight(rightSpeed);
+  driveMotor(MOTOR_L, leftSpeed);
+  driveMotor(MOTOR_R, rightSpeed);
 }
 
 void stopMotors() {
-  digitalWrite(MTR_L_FWD, HIGH); digitalWrite(MTR_L_BWD, HIGH); analogWrite(MTR_L_PWM, 0);
-  digitalWrite(MTR_R_FWD, HIGH); digitalWrite(MTR_R_BWD, HIGH); analogWrite(MTR_R_PWM, 0);
+  driveMotors(0, 0);
 }
-
-// -------------------- TCS34725 sensors --------------------
-
-TwoWire WireLeft(0);
-TwoWire WireRight(1);
-
-Adafruit_TCS34725 tcsLeft(TCS34725_INTEGRATIONTIME_24MS, TCS34725_GAIN_4X);
-Adafruit_TCS34725 tcsRight(TCS34725_INTEGRATIONTIME_24MS, TCS34725_GAIN_4X);
 
 uint16_t readClear(Adafruit_TCS34725 &sensor) {
   uint16_t r, g, b, c;
@@ -73,14 +65,14 @@ uint16_t readClear(Adafruit_TCS34725 &sensor) {
 void setup() {
   Serial.begin(115200);
 
-  pinMode(MTR_L_FWD, OUTPUT); pinMode(MTR_L_BWD, OUTPUT); pinMode(MTR_L_PWM, OUTPUT);
-  pinMode(MTR_R_FWD, OUTPUT); pinMode(MTR_R_BWD, OUTPUT); pinMode(MTR_R_PWM, OUTPUT);
+  pinMode(MOTOR_L.fwd, OUTPUT); pinMode(MOTOR_L.bwd, OUTPUT); pinMode(MOTOR_L.pwm, OUTPUT);
+  pinMode(MOTOR_R.fwd, OUTPUT); pinMode(MOTOR_R.bwd, OUTPUT); pinMode(MOTOR_R.pwm, OUTPUT);
   stopMotors();
 
-  WireLeft.begin(CLR_L_SDA, CLR_L_SCL);
-  WireRight.begin(CLR_R_SDA, CLR_R_SCL);
-  bool leftOk  = tcsLeft.begin(TCS34725_ADDRESS, &WireLeft);
-  bool rightOk = tcsRight.begin(TCS34725_ADDRESS, &WireRight);
+  I2C_L.begin(CLR_L_SDA, CLR_L_SCL);
+  I2C_R.begin(CLR_R_SDA, CLR_R_SCL);
+  bool leftOk  = TCS_L.begin(TCS34725_ADDRESS, &I2C_L);
+  bool rightOk = TCS_R.begin(TCS34725_ADDRESS, &I2C_R);
   Serial.printf("Left Sensor: %s\n", leftOk ? "OK" : "FAIL");
   Serial.printf("Right Sensor: %s\n", rightOk ? "OK" : "FAIL");
 
@@ -95,8 +87,8 @@ void setup() {
 // -------------------- Loop --------------------
 
 void loop() {
-  uint16_t clear_l = readClear(tcsLeft);
-  uint16_t clear_r = readClear(tcsRight);
+  uint16_t clear_l = readClear(TCS_L);
+  uint16_t clear_r = readClear(TCS_R);
 
   bool leftOnLine  = clear_l < LINE_THRESHOLD;
   bool rightOnLine = clear_r < LINE_THRESHOLD;
@@ -105,7 +97,22 @@ void loop() {
   else if (leftOnLine)           driveMotors(-TURN, BASE);  // turn left
   else                           driveMotors(BASE, -TURN);  // turn right
 
-  Serial.printf("CL:%u CR:%u L:%d R:%d\n", clear_l, clear_r, leftOnLine, rightOnLine);
+  static uint32_t lastPrint = 0;
+  uint32_t now = millis();
+  bool print_required = false;
+  if (now - lastPrint >= 1000) {
+    print_required = true;
+    lastPrint = now;
+  }
 
-  delay(loopDelayMs);
+  static bool lastLeftOnLine = false;
+  static bool lastRightOnLine = false;
+  if (leftOnLine != lastLeftOnLine || rightOnLine != lastRightOnLine) {
+    print_required = true;
+    lastLeftOnLine = leftOnLine;
+    lastRightOnLine = rightOnLine;
+  }
+  if (print_required) {
+    Serial.printf("T:%8u  CL:%5u  CR:%5u  L:%d  R:%d\n", now, clear_l, clear_r, leftOnLine, rightOnLine);
+  }
 }
